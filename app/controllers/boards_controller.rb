37787e9a93c19ff834bdc8987186abcdd3aa913b
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-07-16 09:32:25 +0900

    before_action

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,12 @@
 class BoardsController < ApplicationController 
+  
+  before_action :set_board, only: [:show, :edit, :update]
+  
   def index
     @boards = Board.all
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def new
@@ -22,11 +24,9 @@ class BoardsController < ApplicationController
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    @board = Board.find(params[:id])
     if @board.update(board_params)
       redirect_to board_path(@board), notice: '更新できました'
     else
@@ -46,5 +46,9 @@ class BoardsController < ApplicationController
   private
   def board_params
     params.require(:board).permit(:title, :content)
+  end
+
+  def set_board
+    @board = Board.find(params[:id])
   end
 end
