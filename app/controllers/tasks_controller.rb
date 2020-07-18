f9f3c2510faed42cc61bf4ed3c1b6d722186d363
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2020-07-18 13:11:13 +0900

    ボードにタスクを表示できるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,9 @@
 class TasksController < ApplicationController
   
+  def index
+    @task = Task.all
+  end
+
   def new
     board = Board.find(params[:board_id])
     @task = board.tasks.build
@@ -7,7 +11,7 @@ class TasksController < ApplicationController
 
   def create
     board = Board.find(params[:board_id])
-    @task = board.tasks.build(task_params)
+    @task = board.tasks.build(task_params.merge!(user_id: current_user.id))
     if @task.save
       redirect_to board_path(board)
     else
@@ -15,8 +19,13 @@ class TasksController < ApplicationController
     end
   end
 
+  def show
+    board = Board.find(params[:board_id])
+    @task = board.tasks.find(params[:id])
+  end
+
   private
   def task_params
-    params.require(:task).permit(:title, :content, :limit, :user)
+    params.require(:task).permit(:title, :content, :limit)
   end
 end
